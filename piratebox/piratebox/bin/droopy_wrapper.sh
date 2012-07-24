273b38f2075f40d0fab3bd37d82fd9c2546d98d1
Fernando Ortiz <fortiz2k@gmail.com>
2012-07-24 15:44:19 -0500

    change droopy wrapper to show better messages

--- piratebox/piratebox/bin/droopy_wrapper.sh
+++ piratebox/piratebox/bin/droopy_wrapper.sh
@@ -14,7 +14,19 @@ get_pid() {
 
 if [ "$1" == "start" ]; then
     $PIRATEBOX/bin/droopy -H $HOST -d $UPLOADFOLDER -c "" -m "$DROOPY_TXT" $DROOPY_USERDIR  $DROOPY_PORT &
+    if [ $? -gt 0 ]; then
+		echo "droopy failed to start."
+	else
+		echo $! > "${pidfile}"
+      	echo "droopy started."
+    fi
 else
     PID=$(get_pid)
     [ -n "$PID" ] && kill $PID &> /dev/null
+    if [ $? -gt 0 ]; then
+   		echo "droopy failed to stop."
+    else
+   		[ -f "${pidfile}" ] && rm -f "${pidfile}"
+   		echo "droopy stopped."
+    fi
 fi
